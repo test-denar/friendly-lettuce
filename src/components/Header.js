2f2327d453335375689ef79d1282fc05ac112e64
Stackbit Code Editor <projects@stackbit.com>
2021-08-04 12:50:16 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -63,7 +63,6 @@ export default class Header extends React.Component {
     }
 
     render() {
-        console.lgo('test');
         const page = _.get(this.props, 'page');
         const pageUrl = _.trim(getPageUrl(page), '/');
         const config = _.get(this.props, 'config');
